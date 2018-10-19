cb59cd2f060644a31e2986ec2848bb0c8cc83a5c
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-10-20 03:23:34 +0400

    added scripts of UsersDB tables

---
+++ UsersManagement/UsersDB/dbo/Tables/Sessions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Sessions]
+(
+	[Id]			BIGINT			 NOT NULL  IDENTITY(1,1),
+	[UserId]		INT				 NOT NULL,
+	[IpAddress]		NVARCHAR(100)	 NULL,
+	[StartTime]		DATETIME2		 NOT NULL,
+	[EndTime]		DATETIME2		 NOT NULL,
+
+	CONSTRAINT	PK_Session PRIMARY KEY ([Id])
+)
---
+++ UsersManagement/UsersDB/dbo/Tables/Students.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Students]
+(
+	[Id]			INT				NOT NULL,
+	[Department]	NVARCHAR(100)	NULL,
+	[EntranceDate]	DATE			NULL,
+	[ProfileId]		INT				NOT NULL,
+
+	CONSTRAINT		PK_Student		PRIMARY KEY ([Id]),
+	CONSTRAINT		FK_Student_Profile		FOREIGN KEY ([ProfileId]) REFERENCES [dbo].[Profiles]([Id])
+)
