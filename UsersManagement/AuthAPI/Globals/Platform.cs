68d299701097442842b054c2667ab77962b98ce6
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-07 02:50:33 +0400

    added Platform info in AuthAPI for session

---
+++ UsersManagement/AuthAPI/Globals/Platform.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthAPI.Globals
+{
+    /// <summary>
+    /// Enum for describing client platform
+    /// </summary>
+    public enum Platform
+    {
+        iOS,
+        iPadOS,
+        KindleFire,
+        Android,
+        WindowsPhone,
+        Windows7,
+        WindowsVista,
+        WindowsXP,
+        Windows8,
+        Windows81,
+        Windows10,
+        MacOS,
+        Blackberry
+    }
+}
---
+++ UsersManagement/AuthAPI/Models/SessionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AuthAPI.Models
+{
+    /// <summary>
+    /// Class for session
+    /// </summary>
+    public class SessionInfo
+    {
+        /// <summary>
+        /// Gets or sets user id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets start time
+        /// </summary>
+        public DateTimeOffset StartTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets session description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
