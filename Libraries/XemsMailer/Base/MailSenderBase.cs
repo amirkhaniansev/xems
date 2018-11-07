d2e17c5fe5ced4f0bd4ff8183eb69530c7e15ed7
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-07 04:06:01 +0400

    added XemsMailer

---
+++ Libraries/XemsMailer/Base/MailSenderBase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using XemsMailer.Interfaces;
+
+namespace XemsMailer.Base
+{
+    public abstract class MailerSenderBase<TInfo> : IMailer<TInfo>
+    {
+        private readonly NetworkCredential _credentials;
+
+        private readonly SmtpClient _smtpClient;
+
+        private readonly string _host;
+
+        private readonly MailAddress _address;
+
+        private readonly int _port;
+
+        private readonly bool _enableSsl;
+
+        private readonly SmtpDeliveryMethod _deliveryMethod;
+
+        public string Host => this._host;
+
+        public MailAddress FromAddress => this._address;
+
+        public int Port => this._port;
+
+        public bool EnableSsl => this._enableSsl;
+
+        public SmtpDeliveryMethod SmtpDeliveryMethod => this._deliveryMethod;
+
+        public MailerSenderBase(NetworkCredential credentials,
+            string address,
+            string host = "smtp.google.com",
+            int port = 587,
+            bool enableSsl = true,
+            SmtpDeliveryMethod deliveryMethod = SmtpDeliveryMethod.Network)
+        {
+            this._credentials = credentials;
+            this._host = host;
+            this._port = port;
+            this._enableSsl = enableSsl;
+            this._deliveryMethod = deliveryMethod;
+
+            this._address = new MailAddress(address);
+
+            this._smtpClient = new SmtpClient
+            {
+                Host = this._host,
+                Port = this._port,
+                Credentials = this._credentials,
+                EnableSsl = this._enableSsl,
+                DeliveryMethod = this._deliveryMethod
+            };
+        }
+
+        public abstract void Send(string to, string subject, bool IsBodyHtml, TInfo info);
+
+        public abstract void Send(IEnumerable<string> to, string subject, bool IsBodyHtml, TInfo info);
+
+        public abstract Task SendAsync(string to, string subject, bool IsBodyHtml, TInfo info);
+
+        public abstract Task SendAsync(IEnumerable<string> to, string subject, bool IsBodyHtml, TInfo info);
+        
+        protected MailMessage ConstructMail(string to, string subject, bool isBodyHtml)
+        {
+            if(string.IsNullOrEmpty(to))
+                throw new ArgumentNullException("to");
+
+            if (string.IsNullOrEmpty(subject))
+                throw new ArgumentNullException("subject");
+
+            var mail = new MailMessage
+            {
+                From = this._address,
+                Subject = subject,
+                IsBodyHtml = isBodyHtml
+            };
+
+            mail.To.Add(to);
+
+            return mail;
+        }
+    }
+}
---
+++ Libraries/XemsMailer/Interfaces/IMailer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XemsMailer.Interfaces
+{
+    /// <summary>
+    /// Interface fro mailer
+    /// </summary>
+    /// <typeparam name="TInfo"></typeparam>
+    public interface IMailer<in TInfo>
+    {
+        /// <summary>
+        /// Sends information via email
+        /// </summary>
+        /// <param name="to">Email address to which e-mail will be sent.</param>
+        /// <param name="info">Information that will be sent.</param>
+        /// <param name="subject">Subject of email.</param>
+        /// <param name="IsBodyHtml"> Boolean value indicating whether the email has HTML body.</param>
+        void Send(string to, string subject, bool IsBodyHtml, TInfo info);
+
+        /// <summary>
+        /// Sends information via email
+        /// </summary>
+        /// <param name="to">Email addresses to which e-mail will be sent.</param>
+        /// <param name="info">Information that will be sent.</param>
+        /// <param name="subject">Subject of email.</param>
+        /// <param name="IsBodyHtml"> Boolean value indicating whether the email has HTML body.</param>
+        void Send(IEnumerable<string> to, string subject, bool IsBodyHtml, TInfo info);
+
+        /// <summary>
+        /// Sends information via email asynchronously
+        /// </summary>
+        /// <param name="to">Email address to which e-mail will be sent.</param>
+        /// <param name="info">Information that will be sent.</param>
+        /// <returns>task</returns>
+        /// <param name="subject">Subject of email.</param>
+        /// <param name="IsBodyHtml"> Boolean value indicating whether the email has HTML body.</param>
+        Task SendAsync(string to, string subject, bool IsBodyHtml, TInfo info);
+
+        /// <summary>
+        /// Sends information via email
+        /// </summary>
+        /// <param name="to">Email addresses to which e-mail will be sent.</param>
+        /// <param name="info">Information that will be sent.</param>
+        /// <remarks>task</remarks>
+        /// <param name="subject">Subject of email.</param>
+        /// <param name="IsBodyHtml"> Boolean value indicating whether the email has HTML body.</param>
+        Task SendAsync(IEnumerable<string> to, string subject, bool IsBodyHtml, TInfo info);
+    }
+}
