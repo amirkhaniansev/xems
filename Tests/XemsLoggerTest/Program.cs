8718381266587e62c692a691d5ceb107fd658fa8
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-11-08 01:04:35 +0400

    added test of XemsLogger

--- Tests/XemsLoggerTest/Program.cs
+++ Tests/XemsLoggerTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using XemsLogger;
 
 namespace XemsLoggerTest
 {
@@ -6,7 +8,23 @@ namespace XemsLoggerTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new Logger("Test", "E://log.txt", 1);
+
+            for (var counter = 0L; counter < long.MaxValue; counter++)
+            {
+                Thread.Sleep(10);
+
+                var logMsg = $"Log_{counter}";
+
+                var log = new LogInfo
+                {
+                    LogType = LogType.Default,
+                    Message = logMsg,
+                    Time = DateTime.Now
+                };
+
+                logger.Log(logMsg);
+            }
         }
     }
 }
