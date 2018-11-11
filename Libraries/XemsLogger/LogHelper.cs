b6e4c9ed426a49ac06fb20e52be0185f94bd5adf
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-12 02:27:09 +0400

    added LogHelper

---
+++ Libraries/XemsLogger/LogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XemsLogger
+{
+    public class LogHelper
+    {
+        public static LogInfo CreateLog(DateTime? time, LogType logType, string message, Exception exception)
+        {
+            return new LogInfo
+            {
+                Time = time,
+                LogType = logType,
+                Message = message,
+                Exception = exception
+            };
+        }
+    }
+}
