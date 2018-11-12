50288ffba0180d56a42dddf62b25a0baafd84abe
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-12 07:10:47 +0400

    added PasswordHashService test

---
+++ Tests/PasswordHasherTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using XemsPasswordHash;
+
+namespace PasswordHasherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hasher = new PasswordHashService();
+
+            Console.Write("Password : ");
+
+            var password = Console.ReadLine();
+            var passwordHash = hasher.HashPassword(password);
+            
+            Console.WriteLine($"Password Hash: {passwordHash}");
+            Console.ReadLine();
+        }
+    }
+}
