b5da1698e74ecf672c1aeb59b89e248ac69aa9be
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-14 05:34:43 +0400

    added some changes to UsersDb
    added Get sp-s for students
    updated map information
    updated sqlproj file

---
+++ UsersManagement/UsersDB/dbo/StoredProcedures/Students/uspGetStudentByUsername.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspGetStudentByUsername]
+	@username VARCHAR(30)
+AS
+	BEGIN
+		SELECT  s.Id,
+				s.Department,
+				s.EntranceDate,
+				p.CurrentUniversity,
+				p.Occupation,
+				p.UserId
+		FROM [dbo].Students s
+		INNER JOIN [dbo].[Profiles] p ON s.ProfileId = p.Id
+		INNER JOIN [dbo].[Users] u ON p.UserId = u.Id
+		WHERE u.Username = @username
+	END
+RETURN 0
---
+++ UsersManagement/UsersDB/dbo/StoredProcedures/Students/uspGetStudents.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[uspGetStudents]
+AS
+	BEGIN
+		SELECT  s.Id,
+				s.Department,
+				s.EntranceDate,
+				p.CurrentUniversity,
+				p.Occupation,
+				p.UserId
+		FROM [dbo].Students s
+		INNER JOIN [dbo].[Profiles] p ON s.ProfileId = p.Id
+	END
+RETURN 0
