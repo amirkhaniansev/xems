9e8eedb13bc97d42160085e31300a53b46ae1bf2
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-11-24 22:15:39 +0400

    added changes

---
+++ UI/Xems/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Xems.ViewModels
+{
+    public class MainWindowViewModel : XemsViewModelBase
+    {
+        private string _name;
+
+        private string _profilePhotoUrl;
+
+        private string _currentProfileType;
+
+        public string Name
+        {
+            get => this._name;
+
+            set => this.Set("Name", ref this._name, value);
+        }
+
+        public string ProfilePhotoUrl
+        {
+            get => this._profilePhotoUrl;
+
+            set => this.Set("ProfilePhotoUrl", ref this._profilePhotoUrl, value);
+        }
+
+        public string CurrentProfileType
+        {
+            get => this._currentProfileType;
+
+            set => this.Set("CurrentProfileType", ref this._currentProfileType, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            this.Name = "Sevak John Amirkhanian";
+            this._currentProfileType = "Student";
+        }
+    }
+}
---
+++ UI/Xems/ViewModels/XemsViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+using GalaSoft.MvvmLight;
+
+namespace Xems.ViewModels
+{
+    public class XemsViewModelBase : ViewModelBase
+    {
+        private Brush _themeColor;
+
+        public Brush ThemeColor
+        {
+            get => this._themeColor;
+
+            set => this.Set("ThemeColor", ref this._themeColor, value);
+        }
+
+        public XemsViewModelBase()
+        {
+            this._themeColor = (Brush)new BrushConverter().ConvertFrom("#008ABB");
+        }
+    }
+}
